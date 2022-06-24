0f730d732d70b0d0b77661f41217f311ade283bb
wrzonki <wrzonki@gmail.com>
2022-06-25 01:11:50 +0200

    11

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,4 @@
 <script lang="ts">
 	import Input from '$lib/Input.svelte';
-
-	const validate = () => {
-		console.log();
-		console.log('validation');
-		return { error: 'this is an error' };
-	};
-
-	const attributes = {
-		type: 'text'
-	};
 </script>
-
-<Input {attributes} validation={validate()} />
+<Input/>
